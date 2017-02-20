9e2b9e94dc1ca205d48408321f5a0fa5d3694131
Stephen Chin <scapp281@gmail.com>
2017-02-20 16:20:05 -0500

    add landing elements and caption

--- Parallax/js/functions.js
+++ Parallax/js/functions.js
@@ -1,20 +1,24 @@
 var pContainerHeight = $('.bird-box').height();
-
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
-  
     if (wScroll <= pContainerHeight) {
         $('.logo').css({
             'transform': 'translate(0px, ' + wScroll / 2 + '%)'
         });
-
         $('.back-bird').css({
             'transform': 'translate(0px, ' + wScroll / 4 + '%)'
         });
-
         $('.fore-bird').css({
             'transform': 'translate(0px, -' + wScroll / 40 + '%)'
         });
     }
     
-});     
+    /*Landing element*/
+    if (wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+        $('.clothes-pics figure').each(function (i) {
+            setTimeout(function () {
+                $('.clothes-pics figure').eq(i).addClass('is-showing');
+            }, (700 * (Math.exp(i * 0.14))) - 700);
+        });
+    }
+});
