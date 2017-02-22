0000bfafd216d7aa245c69253491f4f6537504eb
Stephen Chin <scapp281@gmail.com>
2017-02-21 21:01:53 -0500

    add promoscope

--- Parallax/js/functions.js
+++ Parallax/js/functions.js
@@ -12,7 +12,7 @@ $(window).scroll(function () {
             'transform': 'translate(0px, -' + wScroll / 40 + '%)'
         });
     }
-    
+
     /*Landing element*/
     if (wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
         $('.clothes-pics figure').each(function (i) {
@@ -20,5 +20,20 @@ $(window).scroll(function () {
                 $('.clothes-pics figure').eq(i).addClass('is-showing');
             }, (700 * (Math.exp(i * 0.14))) - 700);
         });
+    }
+
+    // Promoscope
+    if (wScroll > $('.large-window').offset().top - $(window).height()) {
+
+        $('.large-window').css({
+            'background-position': 'center ' + (wScroll - $('.large-window').offset().top) + 'px'
+        });
+
+        var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+
+        $('.window-tint').css({
+            'opacity': opacity
+        });
+
     }
 });
