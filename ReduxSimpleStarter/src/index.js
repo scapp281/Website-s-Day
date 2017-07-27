e9e0935d29ebad2cf5dab73bd54a71ad63dee7c7
Stephen Chin <scapp281@gmail.com>
2017-07-27 19:38:40 -0400

    add search bar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search-bar';
+
 const  API_KEY = 'AIzaSyBT36JQQtIXFgpCHtAX_DsIZ7LCBsemBdk';
 
 /*Step 1: Create a new component, which produce some HTML.*/
 
-const App = function(){
-    return <div>hi</div>;
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 /* Step 2: Put the component's generated html into DOM.
